3c21456875fdfc6e8dfb309884802c5d5103c3cd
Marcos Paulo <itsolutions031250@gmail.com>
2022-12-07 14:38:13 -0300

    1 - Solução ruim

---
+++ Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modificadorParams
+{
+    internal class Calculator
+    {
+        public static int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public static int Sum(int n1, int n2, int n3)
+        {
+            return n1 + n2 + n3;
+        }
+        public static int Sum(int n1, int n2, int n3, int n4)
+        {
+            return n1 + n2 + n3 + n4;
+        }
+    
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using modificadorParams;
+
+int s1 = Calculator.Sum(20, 30);
+int s2 = Calculator.Sum(20, 30, 40);
+
+Console.WriteLine(s1);
+Console.WriteLine(s2);
